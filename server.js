dc386fff1a6dd8589498bb7e20ac0885b17969c5
ethan-tucker <ethan.tucker.99@gmail.com>
2024-10-27 23:08:20 -0700

    Updating to work with Heroku web hosting

--- server.js
+++ server.js
@@ -373,7 +373,7 @@ app.get('/api/changelogs/status/:id', (req, res) => {
 });
 
 // Start server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
